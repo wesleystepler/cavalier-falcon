cef4b9957b7b5f66fb03a3f40d230b41e664f4a2
Wesley Stepler <pwesleystep@outlook.com>
2022-07-06 20:53:42 -0400

    Modify poker methods and make ranks card attribute

--- Euler/poker_hands/card.py
+++ Euler/poker_hands/card.py
@@ -6,21 +6,21 @@ class Card:
     """Constructor (Default or Overloaded)"""
     def __init__(self, rank=None, suit=None):
         import random
-        ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-        suits = ["H", "D", "S", "C"]
+        self.ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+        self.suits = ["H", "D", "S", "C"]
         if rank and suit:
-            if rank in ranks:
+            if rank in self.ranks:
                 self.rank = rank
             else:
                 raise Exception("Invalid Rank")
             
-            if suit in suits:
+            if suit in self.suits:
                 self.suit = suit
             else:
                 raise Exception("Invalid Suit")
         else:
-            self.rank = random.choice(ranks)
-            self.suit = random.choice(suits)
+            self.rank = random.choice(self.ranks)
+            self.suit = random.choice(self.suits)
         
         self.suit_name = ""
         if self.suit == "H":
--- Euler/poker_hands/poker.py
+++ Euler/poker_hands/poker.py
@@ -22,8 +22,8 @@ def print_hands(hands):
         print(hand)
         print()
 
-#player_hands = deal(deck, 2, 5)
-#print_hands(player_hands)
+player_hands = deal(deck, 2, 5)
+print_hands(player_hands)
 
 
 """
@@ -49,281 +49,152 @@ def high_card(hand1, hand2):
 
 #high_card(player_hands[0], player_hands[1])
 
-def one_pair(hand1, hand2):
+def get_one_pair(hand):
 
-    h1_ranks = []
-    for card in hand1:
-        h1_ranks.append(card.rank)
+    ranks = []
+    for card in hand:
+        ranks.append(card.rank)
     
-    h1_pairs = [rank for rank in h1_ranks if h1_ranks.count(rank) > 1]
+    pairs = [rank for rank in ranks if ranks.count(rank) > 1]
     #print(h1_pairs)
 
-    h1_card_pairs = []
-    for card in hand1:
-        if card.rank in h1_pairs:
-            h1_card_pairs.append(card)
-    #print(h1_card_pairs)
+    card_pairs = []
+    for card in hand:
+        if card.rank in pairs:
+            card_pairs.append(card)
+    return card_pairs
 
-    h2_ranks = []
-    for card in hand2:
-        h2_ranks.append(card.rank)
-
-    h2_pairs = [rank for rank in h2_ranks if h2_ranks.count(rank) > 1]
-    #print(h2_pairs)
-
-    h2_card_pairs = []
-    for card in hand2:
-        if card.rank in h2_pairs:
-            h2_card_pairs.append(card)
-    #print(h2_card_pairs)
-
-    if len(h1_card_pairs) != 0 or len(h2_card_pairs) != 0:
-        if len(h1_card_pairs) == 0:
-            print(f"Player 2 wins with a pair of {max(h2_card_pairs).rank_name}s!")
-        
-        elif len(h2_card_pairs) == 0:
-            print(f"Player 1 wins with a pair of {max(h1_card_pairs).rank_name}s!")
-
-        elif max(h1_card_pairs) > max(h2_card_pairs):
-            print(f"Player 1 wins with a pair of {max(h1_card_pairs).rank_name}s!")
-        
-        elif max(h2_card_pairs) > max(h1_card_pairs):
-            print(f"Player 2 wins with a pair of {max(h2_card_pairs).rank_name}s!")
-        
-        else:
-            print(f"Each Player has a pair of {max(h2_card_pairs).rank_name}s")
+def is_one_pair(cards):
+    if len(cards) >= 2:
+        return True
     else:
-        print("Neither Player has a pair")
+        return False
 
 
-#one_pair(player_hands[0], player_hands[1])
+#print(is_one_pair(get_one_pair(player_hands[0])))
 
-def two_pair(hand1, hand2):
-    h1_ranks = []
-    for card in hand1:
-        h1_ranks.append(card.rank)
+def get_two_pair(hand):
+    ranks = []
+    for card in hand:
+        ranks.append(card.rank)
     
-    h1_pairs = [rank for rank in h1_ranks if h1_ranks.count(rank) > 1]
+    pairs = [rank for rank in ranks if ranks.count(rank) > 1]
     #print(h1_pairs)
 
-    h1_card_pairs = []
-    for card in hand1:
-        if card.rank in h1_pairs:
-            h1_card_pairs.append(card)
-    #print(h1_card_pairs)
+    card_pairs = []
+    for card in hand:
+        if card.rank in pairs:
+            card_pairs.append(card)
+    return card_pairs
 
-    h2_ranks = []
-    for card in hand2:
-        h2_ranks.append(card.rank)
-
-    h2_pairs = [rank for rank in h2_ranks if h2_ranks.count(rank) > 1]
-    #print(h2_pairs)
-
-    h2_card_pairs = []
-    for card in hand2:
-        if card.rank in h2_pairs:
-            h2_card_pairs.append(card)
-    #print(h2_card_pairs)
-
-    if len(h1_card_pairs) >= 4 or len(h2_card_pairs) >= 4:
-        if len(h1_card_pairs) == 0:
-            print(f"Player 2 wins with a pair of {max(h2_card_pairs).rank_name}s and a pair of {min(h2_card_pairs).rank_name}s!")
-        
-        elif len(h2_card_pairs) == 0:
-            print(f"Player 1 wins with a pair of {max(h1_card_pairs).rank_name}s and a pair of {min(h1_card_pairs).rank_name}s!")
-
-        elif max(h1_card_pairs) > max(h2_card_pairs):
-            print(f"Player 1 wins with a pair of {max(h1_card_pairs).rank_name}s and a pair of {min(h1_card_pairs).rank_name}s!")
-        
-        elif max(h2_card_pairs) > max(h1_card_pairs):
-            print(f"Player 2 wins with a pair of {max(h2_card_pairs).rank_name}s and a pair of {min(h2_card_pairs).rank_name}s!")
-        
-        elif min(h1_card_pairs) > min(h2_card_pairs):
-            print(f"Player 1 wins with a pair of {max(h1_card_pairs).rank_name}s and a pair of {min(h1_card_pairs).rank_name}s!")
-
-        elif min(h2_card_pairs) > min(h1_card_pairs):
-            print(f"Player 1 wins with a pair of {max(h1_card_pairs).rank_name}s and a pair of {min(h1_card_pairs).rank_name}s!")
-
-        else:
-            print(f"Each Player has a pair of {max(h1_card_pairs).rank_name}s and a pair of {min(h1_card_pairs).rank_name}s.")
+def is_two_pair(cards):
+    if len(cards) >= 4:
+        return True
     else:
-        print("Neither Player has a two pair")
+        return False
 
-two_pair1 = [Card("J", "C"), Card("K", "D"), Card("J", "D"), Card("K", "H"), Card("4", "S")]
+
+
+two_pair1 = [Card("J", "C"), Card("Q", "D"), Card("J", "D"), Card("K", "H"), Card("4", "S")]
 two_pair2 = [Card("5", "C"), Card("K", "H"), Card("5", "S"), Card("K", "C"), Card("7", "H")]
 two_pair3 = [Card("A", "H"), Card("3", "S"), Card("A", "S"), Card("7", "C"), Card("7", "H")]
-#print(two_pair1)
-#print(two_pair2)
-#print(two_pair3)
-#two_pair(two_pair1, two_pair2)
+#print(is_two_pair((get_two_pair(two_pair1))))
 
 
-def three_of_a_kind(hand1, hand2):
-    h1_ranks = []
-    for card in hand1:
-        h1_ranks.append(card.rank)
+def get_three_of_a_kind(hand):
+    ranks = []
+    for card in hand:
+        ranks.append(card.rank)
 
-    h1_threes = [rank for rank in h1_ranks if h1_ranks.count(rank) > 2]
+    threes = [rank for rank in ranks if ranks.count(rank) > 2]
 
-    h1_card_threes = []
-    for card in hand1:
-        if card.rank in h1_threes:
-            h1_card_threes.append(card)
-    print(h1_card_threes)
+    card_threes = []
+    for card in hand:
+        if card.rank in threes:
+            card_threes.append(card)
+    return card_threes
 
-    h2_ranks = []
-    for card in hand2:
-        h2_ranks.append(card.rank)
-
-    h2_threes = [rank for rank in h2_ranks if h2_ranks.count(rank) > 2]
-
-    h2_card_threes = []
-    for card in hand2:
-        if card.rank in h2_threes:
-            h2_card_threes.append(card)
-    print(h2_card_threes)
-
-    if len(h1_card_threes) >= 3 or len(h2_card_threes) >= 3:
-        if len(h2_card_threes) == 0:
-            print(f"Player 1 wins with three {max(h1_card_threes).rank_name}s!")
-
-        elif len(h1_card_threes) == 0:
-            print(f"Player 2 wins with three {max(h2_card_threes).rank_name}s!")
-
-        elif max(h1_card_threes) > max(h2_card_threes):
-            print(f"Player 1 wins with three {max(h1_card_threes).rank_name}s!")
-        
-        elif max(h2_card_threes) > max(h1_card_threes):
-            print(f"Player 2 wins with three {max(h2_card_threes).rank_name}s!")
-
-        else:
-            print(f"Each Player has three {max(h1_card_threes).rank}s.")
-
+def is_three_of_a_kind(cards):
+    if len(cards) >= 3:
+        return True
     else:
-        print("Neither Player has three of a kind.")
+        return False
 
-three1 = [Card("4", "C"), Card("4", "H"), Card("A", "D"), Card("4", "S"), Card("6", "C")]
+
+three1 = [Card("4", "C"), Card("2", "H"), Card("A", "D"), Card("4", "S"), Card("6", "C")]
 three2 = [Card("7", "C"), Card("7", "S"), Card("K", "H"), Card("7", "D"), Card("9", "S")]
 
-#three_of_a_kind(three1, three2)
+#print(is_three_of_a_kind(get_three_of_a_kind(three1)))
 
-def straight(hand1, hand2):
-    hand1.sort()
-    for i in range(1, len(hand1)):
-        if hand1[i] > hand1[i-1]:
-            hand1_straight = True
+def is_straight(hand):
+    hand.sort()
+    for i in range(1, len(hand)):
+        if hand[i].ranks.index(hand[i].rank) == hand[i-1].ranks.index(hand[i-1].rank) + 1:
+            straight = True
         else:
-            hand1_straight = False
+            straight = False
             break
+
+    return straight
     
-    hand2.sort()
-    for i in range(1, len(hand2)):
-        if hand2[i] > hand2[i-1]:
-            hand2_straight = True
-        else:
-            hand2_straight = False
-            break
-
-    if hand1_straight and hand2_straight:
-        if max(hand1) > max(hand2):
-            print("Player 1 wins on a Straight!")
-
-        elif max(hand2) > max(hand1):
-            print("Player 2 wins on a Straight!")
-        
-        else:
-            print("Players have the same Straight!")
-
-    elif hand1_straight:
-        print("Player 1 wins on a Straight!")
-
-    elif hand2_straight:
-        print("Player 2 wins on a Straight!")
 
 straight1 = [Card("8", "H"), Card("6", "C"), Card("10", "D"), Card("7", "S"), Card("9", "H")]
 straight2 = [Card("J", "D"), Card("A", "H"), Card("K", "S"), Card("10", "C"), Card("Q", "D")]
 straight3 = [Card("5", "D"), Card("A", "H"), Card("7", "S"), Card("10", "C"), Card("Q", "D")]
-#straight(straight3, straight1)
+#print(is_straight(straight1))
 
-def flush(hand1, hand2):
-    suit = hand1[0].suit
-    for card in hand1:
+def is_flush(hand):
+    suit = hand[0].suit
+    for card in hand:
         if card.suit == suit:
-            h1_flush = True
+            flush = True
         else:
-            h1_flush = False
+            flush = False
             break
+    return flush
 
-    suit = hand2[0].suit
-    for card in hand2:
-        if card.suit == suit:
-            h2_flush = True
-        else:
-            h2_flush = False
-            break
-
-    if h1_flush and h2_flush:
-        if max(hand1) > max(hand2):
-            print("Player 1 wins on a Flush!")
-        elif max(hand2) > max(hand1):
-            print("Player 2 wins on a Flush!")
-        else:
-            print("Players have the same Flush")
-
-    elif h1_flush:
-        print("Player 1 wins on a Flush!")
-
-    elif h2_flush:
-        print("Player 2 wins on a Flush!")
 
 
 flush1 = [Card("5", "H"), Card("10", "H"), Card("A", "H"), Card("2", "H"), Card("9", "H")]
 flush2 = [Card("4", "C"), Card("7", "C"), Card("K", "C"), Card("J", "C"), Card("9", "C")]
 flush3 = [Card("5", "H"), Card("10", "H"), Card("A", "H"), Card("2", "H"), Card("9", "H")]
 
-flush(flush1, flush3)
+#print(is_flush(flush1))
 
-def four_of_a_kind(hand1, hand2):
-    h1_ranks = []
-    for card in hand1:
-        h1_ranks.append(card.rank)
-
-    h1_four = [rank for rank in h1_ranks if h1_ranks.count(rank) > 3]
-
-    h1_card_four = []
-    for card in hand1:
-        if card.rank in h1_four:
-            h1_card_four.append(card)
-
-    h2_ranks = []
-    for card in hand2:
-        h2_ranks.append(card.rank)
-
-    h2_four = [rank for rank in h2_ranks if h2_ranks.count(rank) > 3]
-
-    h2_card_four = []
-    for card in hand2:
-        if card.rank in h2_four:
-            h2_card_four.append(card)
-
-    if len(h1_card_four) >= 3 or len(h2_card_four) >= 3:
-        if len(h2_card_four) == 0:
-            print(f"Player 1 wins with four {max(h1_card_four).rank_name}s!")
-
-        elif len(h1_card_four) == 0:
-            print(f"Player 2 wins with four {max(h2_card_four).rank_name}s!")
-
-        elif max(h1_card_four) > max(h2_card_four):
-            print(f"Player 1 wins with four {max(h1_card_four).rank_name}s!")
-        
-        elif max(h2_card_four) > max(h1_card_four):
-            print(f"Player 2 wins with four {max(h2_card_four).rank_name}s!")
-
-        else:
-            print(f"Each Player has four {max(h1_card_four).rank}s.")
-
+def is_full_house(hand):
+    toak = get_three_of_a_kind(hand)
+    op = get_one_pair(hand)
+    print(get_three_of_a_kind(hand), get_one_pair(hand))
+    if is_three_of_a_kind(toak) and is_one_pair(op):
+        return True
     else:
-        print("Neither Player has four of a kind.")
+        return False
+
+fhouse = [Card("10", "D"), Card("K", "D"), Card("4", "H"), Card("K", "S"), Card("K", "C")]
+print(is_full_house(fhouse))
+
+def get_four_of_a_kind(hand):
+    ranks = []
+    for card in hand:
+        ranks.append(card.rank)
+
+    four = [rank for rank in ranks if ranks.count(rank) > 3]
+
+    card_four = []
+    for card in hand:
+        if card.rank in four:
+            card_four.append(card)
+    return card_four
+
+def is_four_of_a_kind(cards):
+    if len(cards) == 4:
+        return True
+    else:
+        return False
+
+four1 = [Card("7", "H"), Card("10", "D"), Card("8", "S"), Card("7", "C"), Card("7", "D")]
+#print(is_four_of_a_kind((get_four_of_a_kind(four1))))
+
 
 
 
