fdfd53e289c598d083ea17824c9031a343435943
Wesley Stepler <pwesleystep@outlook.com>
2022-10-10 15:20:18 -0400

    First go at moving boxes - does not work

--- cs3100/module_4/moving/moving.py
+++ cs3100/module_4/moving/moving.py
@@ -1,39 +1,56 @@
 #NOTE: Python rounds down for integer division, so you will need to add 1 when you get to that point
 from shipping import Shipping
-#test_cases = input()
-
-"""data = [int(i) for i in input().split() if i.isdigit()]
-box_move, box_take, companies = data[0], data[1], data[2]
-box_ship = box_move - box_take
-
-shipping_list = []
-for i in range(0, companies):
-    data = input().split()
-    s = Shipping(data[0], data[1], data[2])
-    shipping_list.append(s)"""
-
+from shipping import OptimalShipping
+import math
 
 def best_shipping(company, box_ship):
     cost = 0
+    if company.cost_half == 0 or company.cost_one == 0:
+        result = OptimalShipping(company.name, 0)
+        return result
     while box_ship > 0:
-        box_half = int(box_ship/2) + 1
-        ship_half = box_half/company.cost_half
+        # On each pass, we want the option that packs the most boxes for least cost
+        
+        box_half = math.ceil(box_ship/2)
+    
+        ship_half = company.cost_half/box_half
         ship_one = company.cost_one/1
-        if ship_half > ship_one:
+        if ship_half < ship_one:
             cost += company.cost_half
             box_ship -= box_half
         else:
             cost += company.cost_one
             box_ship -= 1
-    cost += company.cost_one
-    return cost
 
-c = Shipping("UHAUL", 3, 2)
-print(best_shipping(c, 65))
+    result = OptimalShipping(company.name, cost)
+    return result
+
+
+
+test_cases = input()
+for j in range(0, int(test_cases)):
+    data = [int(i) for i in input().split() if i.isdigit()]
+    box_move, box_take, companies = data[0], data[1], data[2]
+    box_ship = box_move - box_take
+
+    shipping_list = []
+    for i in range(0, companies):
+        data = input().split()
+        s = Shipping(data[0], int(data[1]), int(data[2]))
+        shipping_list.append(s)
+
+    shipping_cost = []
+    for i in range(0, len(shipping_list)):
+        shipping_cost.append(best_shipping(shipping_list[i], box_ship))
+
+    shipping_cost.sort()
+    print(f"Case {j+1}")
+    for i in range(0, len(shipping_cost)):
+        print(shipping_cost[i])
+
             
-
-
-
+# Services with larger half costs are returning bigger numbers than they should
+# Services with larger one costs are returning smaller numbers than they should
     
 
 
--- cs3100/module_4/moving/shipping.py
+++ cs3100/module_4/moving/shipping.py
@@ -2,11 +2,52 @@ class Shipping():
     def __new__(cls, *args, **kwargs):
         return super().__new__(cls)
 
-    def __init__(self, company="", cost_one=0, cost_half =0):
-        self.company = company
+    def __init__(self, name="", cost_one=0, cost_half =0):
+        self.name = name
         self.cost_one = cost_one
         self.cost_half = cost_half
 
 
     def __repr__(self):
-        return f"{self.company}, Ship one box for ${self.cost_one}, half the boxes for ${self.cost_half}"
+        return f"{self.company}"
+
+
+class OptimalShipping():
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls)
+
+    def __init__(self, company="", cost=0):
+        self.company = company
+        self.cost = cost
+
+    def __gt__(self, other):
+        if self.cost > other.cost:
+            return True
+        elif self.cost == other.cost:
+            if self.company > other.company:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def __lt__(self, other):
+        if self.cost < other.cost:
+            return True
+        elif self.cost == other.cost:
+            if self.company < other.company:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def __eq__(self, other):
+        if self.cost == other.cost:
+            return True
+        else:
+            return False
+
+
+    def __repr__(self):
+        return f"{self.company} {self.cost}"
