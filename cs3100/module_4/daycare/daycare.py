9256d9d16e4ab24cacd10421e89baffc34d510e4
Wesley Stepler <pwesleystep@outlook.com>
2022-10-26 11:28:34 -0400

    making progress on daycare

--- cs3100/module_4/daycare/daycare.py
+++ cs3100/module_4/daycare/daycare.py
@@ -1,16 +1,72 @@
+from ssl import DER_cert_to_PEM_cert
 from room import Room
-import math
 
-# Greedy choice for the growing rooms: pick the original room size that has the biggest upgrade
+# Greedy choice for the growing rooms: pick the old capacity of the room with the largest new capacity
 # Greedy choice for staying the same size: pick the largest room
-# Greedy choice for all shrinking: largest original size 
+# Greedy choice for all shrinking: Pick room with largest new size first. 
+
+"""def sort_by_old_max(room_list):
+    room_sizes = []
+    sorted_list = []
+    for i in range(0, len(room_list)):
+        room_sizes.append(room_list[i].old_max)
+    room_sizes.sort()
+    for size in room_sizes:
+        for i in range(0, len(room_list)):
+            if room_list[i].old_max == size:
+                sorted_list.append(room_list[i])
+                break
+    return sorted_list"""
+
+
+# Where I stopped: working on getting the code to properly handle when all rooms decrease.
+# General idea is to iterate through, and if at any point all rooms and the trailer are full, add
+# to the total capacity needed for the trailer. 
+def handle_decrease(decreasing):
+    trailer = 0
+    new_cap = 0
+    new_max = 0
+
+
+    for i in range(0, len(decreasing)):
+        # Relocate all the kids to where they need to go
+        while decreasing[i].old_max > 0:
+            decreasing[i].old_max -= 1
+            if new_max > 0 and new_cap < new_max:
+                new_cap += 1
+            else:
+                trailer += 1
+        new_max += decreasing[i].new_max
+        while new_cap < new_max and trailer > 0:
+            trailer -= 1
+            new_cap += 1
+    return trailer
+
+def handle_increase(increasing):
+    max_diff = 0
+    room = Room()
+    for i in range(0, len(increasing)):
+        if increasing[i].new_max - increasing[i].old_max > max_diff:
+            max_diff = (increasing[i].new_max - increasing[i].old_max)
+            room = increasing[i]
+    return room.old_max
+
+
+def handle_same(same):
+    max_size = 0
+    for i in range(0, len(same)):
+        if same[i].old_max > max_size:
+            max_size = same[i].old_max
+    return max_size
+
+        
 
 rooms = []
 
 increasing = []
 same = []
 decreasing = []
-
+solution = 0
 
 for i in range(0,2):
     num_rooms = int(input())
@@ -28,52 +84,32 @@ for i in range(0,2):
     max_diff = 0
     r = Room()
 
-    if len(increasing) == 0 and len(same) == 0 and len(decreasing) > 0:
-        #decreasing.sort()
-        solution = 0
-        for i in range(0, len(decreasing)):
-            if decreasing[i].old_max > solution:
-                solution = decreasing[i].old_max
-
-        for i in range(1, len(decreasing)):
-            solution += (abs(decreasing[i].new_max - decreasing[i].old_max))
-        print(solution)
-        
-    elif len(increasing) == 0 and len(same) == 0 and len(decreasing) == 0:
-        solution = 0
-        for i in range(0, len(rooms)):
-            solution += rooms[i].old_max
-        print(solution)
-    else:
-        for i in range(0, len(increasing)):
-            diff = increasing[i].new_max - increasing[i].old_max
-            if diff > max_diff:
-                max_diff = diff
-                r = increasing[i]
-            elif diff == max_diff:
-                if increasing[i].old_max < r.old_max:
-                    r = increasing[i]
-
-        for i in range(0, len(same)):
-            if same[i].old_max > max_diff:
-                max_diff = same[i].old_max
-                r = same[i]
-            elif same[i].old_max == max_diff:
-                if same[i].old_max < r.old_max:
-                    r = same[i]
+    increasing.sort(reverse=True)
+    same.sort()
+    decreasing.sort(reverse=True)
 
 
-        for i in range(0, len(decreasing)):
-            diff = decreasing[i].old_max - decreasing[i].new_max 
-            if diff > max_diff:
-                max_diff = decreasing[i].old_max
-                r = decreasing[i]
-            elif diff == max_diff:
-                if decreasing[i].old_max < r.old_max:
-                    r = same[i]
+    results = []
+    if len(increasing) > 0:
+        i = handle_increase(increasing)
+        results.append(i)
+    if len(same) > 0:
+        s = handle_same(same)
+        results.append(s)
+    if len(decreasing) > 0:
+       d = handle_decrease(decreasing)
+       results.append(d)
 
-        print(r.old_max)
-        
+    # Need to handle case where two maxes are the same so that it picks the smaller starting
+    # trailer size, not the bigger one. 
+    print(max(results))
+
     increasing.clear()
     same.clear()
     decreasing.clear()
+
+    
+
+
+
+
--- cs3100/module_4/daycare/room.py
+++ cs3100/module_4/daycare/room.py
@@ -2,19 +2,20 @@ class Room():
     def __new__(cls, *args, **kwargs):
         return super().__new__(cls)
 
-    def __init__(self, name="", old_max=0, new_max=0):
+    def __init__(self, name="", old_max=0, new_max=0, cur_capacity=0):
         self.name = name
         self.old_max = old_max
         self.new_max = new_max
+        self.cur_capacity = cur_capacity
 
     def __gt__(self, other):
-        if self.old_max > other.old_max:
+        if self.new_max > other.new_max:
             return True
         else:
             return False
 
     def __lt__(self, other):
-        if self.old_max < other.old_max:
+        if self.new_max < other.new_max:
             return True
         else:
             return False
