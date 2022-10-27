d1ffac4db4203733efc6ead8c5d8712549a10549
Wesley Stepler <pwesleystep@outlook.com>
2022-10-27 10:26:37 -0400

    finish mod 4 homeworks

--- cs3100/module_4/daycare/daycare.py
+++ cs3100/module_4/daycare/daycare.py
@@ -1,20 +1,25 @@
 from room import Room
 
-# Greedy choice for the growing rooms: pick the old capacity of the room with the largest new capacity
+# Greedy choice for the growing rooms: pick the smallest old room
 # Greedy choice for staying the same size: pick the largest room
 # Greedy choice for all shrinking: Pick room with largest new size first. 
 
 def sort_by_old_max(room_list):
     room_sizes = []
     sorted_list = []
+
     for i in range(0, len(room_list)):
         room_sizes.append(room_list[i].old_max)
+
     room_sizes.sort()
     for size in room_sizes:
-        for i in range(0, len(room_list)):
+        i = 0
+        while len(room_list) > 0:
             if room_list[i].old_max == size:
                 sorted_list.append(room_list[i])
+                room_list.remove(room_list[i])
                 break
+            i += 1
     return sorted_list
 
 
@@ -39,40 +44,17 @@ def handle_moving(room_list):
             trailer -= 1
             new_cap += 1
     return trailer_max
-
-def handle_increase(increasing):
-    max_new = 0
-    room = Room()
-    for i in range(0, len(increasing)):
-        if increasing[i].max_new > max_new:
-            max_new = increasing[i].max_new
-    return max_new
-        
-
-
-def handle_same(same):
-    max_size = 0
-    room = Room()
-    for i in range(0, len(same)):
-        if same[i].old_max > max_size:
-            max_size = same[i].old_max
-            room = same[i]
-    return max_size
-
-        
-
+      
 rooms = []
-
 increasing = []
 same = []
 decreasing = []
-solution = 0
 
 for i in range(0,2):
     num_rooms = int(input())
     for i in range(0, num_rooms):
         cap = input().split()
-        room = Room(f"Room {i+1}", int(cap[0]), int(cap[1]))
+        room = Room(f"Room {chr(65+i)}", int(cap[0]), int(cap[1]))
         rooms.append(room)
         if room.new_max - room.old_max > 0:
             increasing.append(room)
@@ -81,14 +63,9 @@ for i in range(0,2):
         else:
             same.append(room)
 
-    max_diff = 0
-    r = Room()
-
-    increasing.sort(reverse=True)
-    #print(increasing)
+    increasing = sort_by_old_max(increasing)
     same.sort()
     decreasing.sort(reverse=True)
-
 
     sorted_list = []
     for j in range(0, len(increasing)):
@@ -99,25 +76,9 @@ for i in range(0,2):
 
     for j in range(0, len(decreasing)):
         sorted_list.append(decreasing[j])
-    
-    """if len(increasing) > 0:
-        print(increasing[0].old_max)
-
-    if len(same) > 0:
-        s = handle_same(same)
-        results.append(s)
-    if len(decreasing) > 0:
-       d = handle_moving(decreasing)
-       results.append(d)"""
 
     print(handle_moving(sorted_list))
 
     increasing.clear()
     same.clear()
     decreasing.clear()
-
-    
-
-
-
-
--- cs3100/module_4/daycare/room.py
+++ cs3100/module_4/daycare/room.py
@@ -2,11 +2,11 @@ class Room():
     def __new__(cls, *args, **kwargs):
         return super().__new__(cls)
 
-    def __init__(self, name="", old_max=0, new_max=0, cur_capacity=0):
+    def __init__(self, name="", old_max=0, new_max=0):
         self.name = name
         self.old_max = old_max
         self.new_max = new_max
-        self.cur_capacity = cur_capacity
+        self.diff = new_max - old_max
 
     def __gt__(self, other):
         if self.new_max > other.new_max:
