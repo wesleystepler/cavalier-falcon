31b662ebd0e29408151bb6acd37aec09de408ddd
Wesley Stepler <pwesleystep@outlook.com>
2022-11-02 17:38:57 -0400

    recursion is stupid

--- cs3100/module_5/drainage.py
+++ cs3100/module_5/drainage.py
@@ -15,49 +15,76 @@ for row in grid:
     print(row)
 
 all_paths = []
+completed = []
 def grid_search(grid, index_a, index_b, visited):
     global all_paths
-    for i in range(index_a, len(grid)):
-        for j in range(index_b, len(grid[i])):
-            print(j)
-            visited.append(grid[i][j])
-            current = grid[i][j]
-            if current == 78:
+    global completed
+    
+    while index_a < len(grid):
+        while index_b < len(grid[i]):
+            print(index_a, index_b)
+            visited.append(grid[index_a][index_b])
+            current = grid[index_a][index_b]
+            if current == 7:
                 print()
             neighbors = []
-            if i < len(grid) - 1:
-                neighbors.append(grid[i+1][j])
-            if j < len(grid[i]) - 1:
-                neighbors.append(grid[i][j+1])
-            if i > 0:
-                neighbors.append(grid[i-1][j])
-            if j > 0:
-                neighbors.append(grid[i][j-1])
+            if index_a < len(grid) - 1:
+                neighbors.append(grid[index_a+1][index_b])
+            if index_b < len(grid[index_a]) - 1:
+                neighbors.append(grid[index_a][index_b+1])
+            if index_a > 0:
+                neighbors.append(grid[index_a-1][index_b])
+            if index_b > 0:
+                neighbors.append(grid[index_a][index_b-1])
             counter = 0
             for num in neighbors:
                 if current < num:
                     counter += 1
             if counter == len(neighbors):
                 all_paths.append(visited.copy())
-                return visited
+                if len(visited) > 1:
+                    return visited
+                
 
-            elif grid[i+1][j] < current:
-                grid_search(grid, index_a+1, index_b, visited)
-                visited.clear()
-            
-            elif grid[i][j+1] < current:
-                grid_search(grid, index_a, index_b+1, visited)
-                visited.clear()
+            else:
 
-            elif grid[i][j-1] < current and j > 0:
-                index_b -= 1
-                grid_search(grid, index_a, index_b-1, visited)
-                visited.clear()
+                if index_a + 1 < len(grid):
+                    if grid[index_a+1][index_b] < current:
+                        temp1 = index_a
+                        temp2 = index_b
+                        grid_search(grid, index_a+1, index_b, visited)
+                        index_a = temp1
+                        index_b = temp2
+                        visited.pop()
+                if index_b+1 < len(grid[index_a]):
+                    if grid[index_a][index_b+1] < current:
+                        temp1 = index_a
+                        temp2 = index_b
+                        grid_search(grid, index_a, index_b+1, visited)
+                        index_a = temp1
+                        index_b = temp2
+                        visited.pop()
+                if index_b - 1 >= 0:
+                    if grid[index_a][index_b-1] < current:
+                        temp1 = index_a
+                        temp2 = index_b
+                        grid_search(grid, index_a, index_b-1, visited)
+                        index_a = temp1
+                        index_b = temp2
+                        visited.pop()
+                if index_a - 1 >= 0:
+                    if grid[index_a-1][index_b] < current:
+                        temp1 = index_a
+                        temp2 = index_b
+                        grid_search(grid, index_a-1, index_b, visited)
+                        index_a = temp1
+                        index_b = temp2
+                        visited.pop()
+            visited.clear()
 
-            elif grid[i-1][j] < current and i > 0:
-                index_a -= 1
-                grid_search(grid, index_a-1, index_b, visited)
-                visited.clear()
+            index_b += 1
+        index_a += 1
+    return "Done"
 
 print(grid_search(grid, 0, 0, []))
 print(all_paths)
