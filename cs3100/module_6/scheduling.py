b45cdefe12d6f7114f0324bd65fd2a95d718c7c3
Wesley Stepler <pwesleystep@outlook.com>
2022-12-03 22:23:41 -0500

    help

--- cs3100/module_6/scheduling.py
+++ cs3100/module_6/scheduling.py
@@ -4,10 +4,19 @@ c = nums[1] # Number of total available courses
 n = nums[2] # Number of courses each student needs to enroll in
 
 # Parse input
-index_tracker = {}
+index_tracker = {} # Track what students/courses etc. are at each index
+reqs_tracker = {} # Track the classes students what to take, and the class capacities
+capacities = {}
 
-reqs_tracker = {}
+enrolled = {} # Track the number of courses each student is enrolled in - used later
 index = 0
+
+# Source: https://note.nkmk.me/en/python-dict-get-key-from-value/
+def get_key_from_value(d, val):
+    keys = [k for k, v in d.items() if v == val]
+    if keys:
+        return keys[0]
+    return None
 
 for i in range(0, r):
     s = input().split()
@@ -33,13 +42,17 @@ for i in range(0, c):
     if reqs_tracker.get(s[0]) == None:
         reqs_tracker[s[0]] = s[1]
 
+    ind = get_key_from_value(index_tracker, s[0])
+    capacities[ind] = int(s[1])
+
 num_courses = len(index_tracker) - num_students
 
 index_tracker[len(index_tracker)] = "Source"
 index_tracker[len(index_tracker)] = "Sink"
 
-print(index_tracker)
-print(reqs_tracker)
+#print(index_tracker)
+#print(reqs_tracker)
+#print(capacities)
 
 
 # Use input to construct graph, stored in an adjacency matrix.
@@ -66,28 +79,72 @@ for j in range(0, num_students):
 for row in graph:
     print(row)
 
-print(num_students)
-print(num_courses)
+#print(num_students)
+#print(num_courses)
 
 # Implement Ford-Fulkerson
 # All augmenting paths will have this form:
 # Source --> Student --> Class --> Sink
-is_path = False
-def dfs(graph, start, end, visited):
+is_path = True
+def dfs(start, end, visited, num_students, num_courses, capacities):
+    global graph
     global is_path
-    if len(graph) > 1:
-        visited.append(start)
-        if start == end:
+    path = ""
+    stack = []
+    stack.append(start)
+    while len(stack) > 0:
+        i = stack[-1]
+        stack.remove(stack[-1])
+        path += f"{i}-"
+        if i == end:
             is_path = True
-            return True
-        else:
-            for i in range(0, len(graph[start])):
-                if i not in visited and graph[start][i] != 0:
-                    v = i
-                    dfs(graph, v, end, visited)
+            break
+        visited.append(i)
+        s = 0
+        if i >= num_students and i < num_courses + num_students:
+            s = num_students + num_courses
+        elif i < num_students:
+            s = num_students
+        for j in range(s, len(graph[i])):
+            if j >= num_students and j <= num_students + num_courses:
+                if j not in visited and graph[i][j] != 0 and graph[j][len(graph)-1] != 0:
+                    graph[i][j] -= 1
+                    graph[j][i] += 1
 
-dfs(graph, 7, len(graph)-1, [])
-print(is_path)
+                    stack.append(j)
+                    break
+            else:
+                if j not in visited and graph[i][j] != 0:
+                    graph[i][j] -= 1
+                    graph[j][i] += 1
+
+                    stack.append(j)
+                    break
+
+reqs_fulfilled = 0
+while is_path:
+    is_path = False
+    dfs(len(graph)-2, len(graph)-1, [], num_students, num_courses, capacities)
+    if is_path:
+        reqs_fulfilled += 1
+
+all_zeroes = True
+row = len(graph)-2
+for i in range(0, len(graph[row])):
+    if graph[row][i] != 0:
+        all_zeroes = False
+        break
+if reqs_fulfilled == num_students*n and not all_zeroes:
+    print("Yes")
+else:
+    print("No")
+for row in graph:
+    print(row)
+
+#print(num_students)
+#print(num_courses)
+
+
 
 
 
