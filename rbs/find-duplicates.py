d157f416f59f61e45686a0ded61cc4251b74bcc2
Wesley Stepler <pwesleystep@outlook.com>
2023-07-06 16:00:49 -0400

    first draft is working

--- rbs/find-duplicates.py
+++ rbs/find-duplicates.py
@@ -1,50 +1,47 @@
 from openpyxl import load_workbook
 from difflib import SequenceMatcher
-workbook = load_workbook(filename='test-data.xlsx', read_only=True)
-sheet = workbook.active
 
-account_names = []
-row, col = 3, 5
-val = sheet.cell(row=row, column=col).value
-while val != None:
-    #print(val)
-    account_names.append(val)
-    row += 1
-    val = sheet.cell(row=row, column=col).value
+workbook = load_workbook(filename='rbs-report.xlsx', data_only=True)
+sheet = workbook.active
 
 def compare_strings(a: str, b: str):
     return SequenceMatcher(None, a.lower(), b.lower()).ratio()
 
-def find_duplicates(acc_names):
+def compare_words(a, b):
+    a_split = a.split()
+    b_split = b.split()
+
+def find_duplicates(sheet):
+    row, col = 12, 5
     potential_duplicates = []
-    while len(acc_names) > 0:
-        base = acc_names[0]
-        base_in_list = False
-        for i in range(1, len(acc_names)):
+    visited_cells = []
+    while sheet.cell(row, col).value != None:
+        c1 = sheet.cell(row, col)
+        c1_val = sheet.cell(row, col).value
+        i, j, = row+1, col
+        while sheet.cell(i, j).value != None:
+            print(i, j)
             similar = False
-            print(base, acc_names[i])
-            if acc_names[i] == base:
+            c2 = sheet.cell(i, j)
+            c2_val = sheet.cell(i, j).value
+            if c1_val == c2_val and c1 != c2:
                 similar = True
-            elif compare_strings(base, acc_names[i]) > 0.85:
+            
+            elif compare_strings(c1_val, c2_val) > 0.85:
                 similar = True
 
             if similar:
-                if not base_in_list:
-                    potential_duplicates.extend([base, acc_names[i]])
-                    base_in_list = True
-                else:
-                    potential_duplicates.append(acc_names[i])
-        acc_names.remove(acc_names[0])
+                if c1 not in visited_cells:
+                    potential_duplicates.append(c1_val)
+                    visited_cells.append(c1)
+                if c2 not in visited_cells:
+                    potential_duplicates.append(c2_val)
+                    visited_cells.append(c2)
+            i += 1
+        row += 1
 
     return potential_duplicates
 
-print(account_names)
-print(find_duplicates(account_names))
-
-
-
-    
-
-# Test, test1, the test, tests, your mom, test
+print(find_duplicates(sheet))
 
 
